83194b14d594201148f1c695d5b7008d9b674a30
altasB2 <altas@base2.my>
2022-02-10 17:33:21 +0800

    Updated options code

---
+++ app/Models/Option.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
+class Option extends Model
+{
+    use HasFactory, LogsActivity, SoftDeletes;
+
+    protected $fillable = [
+        'option_name',
+        'option_value',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'deleted_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    protected static $logAttributes = [
+        'option_name',
+        'option_value',
+    ];
+
+    protected static $logName = 'options';
+
+    public function getActivitylogOptions(): LogOptions {
+        return LogOptions::defaults()->logFillable();
+    }
+
+    public function getDescriptionForEvent( string $eventName ): string {
+        return "{$eventName} option";
+    }
+}
