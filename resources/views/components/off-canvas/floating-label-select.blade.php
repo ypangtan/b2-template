f65168248bcdd3719a07ad390717acd655329f7b
altasB2 <altas@base2.my>
2022-09-17 01:09:47 +0800

    Floating label select_x

--- resources/views/components/off-canvas/floating-label-select.blade.php
+++ resources/views/components/off-canvas/floating-label-select.blade.php
@@ -1,6 +1,6 @@
 <div class="form-floating mb-3">
     <select {{ $attributes }} >
-        <option value="" selected>{{ __( 'administrator.select_x', [ 'role' => $title ] ) }}</option>
+        <option value="" selected>{{ __( 'datatables.select_x', [ 'title' => $title ] ) }}</option>
         @foreach( json_decode( html_entity_decode( $options ) ) as $option )
         <option value="{{ $option->value }}">{{ $option->title }}</option>
         @endforeach
