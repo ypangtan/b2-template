a6838c4245da907e35735648df4cd2691c2dcc24
altasB2 <altas@base2.my>
2023-03-28 17:18:29 +0800

    Seeder password

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,7 +18,7 @@ class AdminSeeder extends Seeder
         DB::table( 'admins' )->insert([
             'username' => 'altasB2',
             'email' => 'altas@base2.my',
-            'password' => Hash::make( 'abcd1234' ),
+            'password' => Hash::make( 'altas1234' ),
             'name' => 'Altas Xiao',
             'role' => 1,            
             'created_at' => date( 'Y-m-d H:i:s' ),
@@ -37,16 +37,5 @@ class AdminSeeder extends Seeder
             'model_type' => 'App\Models\Admin',
             'model_id' => 1,
         ] );
-
-        // DB::table( 'modules' )->insert( [
-        //     [ 'name' => 'admins', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'customers', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'suppliers', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'orders', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'categories', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'products', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'wallets', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        //     [ 'name' => 'settings', 'created_at' => date( 'Y-m-d H:i:s' ), 'updated_at' => date( 'Y-m-d H:i:s' ) ],
-        // ] );
     }
 }
