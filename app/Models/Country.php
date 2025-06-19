418401e9152e843fee0f7f20e4a3de3cebde0733
YanPang <nicholastanyanpang@gmail.com>
2025-06-19 17:21:17 +0800

    updateCountry

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\File;
 
 use App\Traits\HasTranslations;
 
@@ -18,8 +17,6 @@ class Country extends Model
 
     protected $appends = [
         'encrypted_id',
-        'image_icon',
-        'image_path',
     ];
 
     public $translatable = [ 'country_name' ];
@@ -36,38 +33,7 @@ class Country extends Model
         'status',
     ];
     
-    public function document()
-    {
-        return $this->belongsToMany(Document::class);
-    }
-    
     public function getEncryptedIdAttribute() {
         return \Helper::encode( $this->attributes['id'] );
-    }
-
-    public function getImageIconAttribute() {
-        return $this->attributes[ 'country_icon' ] ? asset('vendor/blade-flags/' . $this->attributes[ 'country_icon' ] ) : null;
-    }
-
-    public function getImagePathAttribute() {
-        return $this->attributes[ 'country_image' ] ? asset( 'country/image_medium/' . $this->attributes[ 'country_image' ] ) : null;
-    }
-
-    public function getGotImageAttribute() {
-        if( !$this->attributes[ 'country_image' ] ) {
-            return false;
-        }
-
-        $imagePath = public_path('country/image_medium/' . $this->attributes[ 'country_image' ] );
-        return File::exists($imagePath);
-    }
-
-    public function getGotIconAttribute() {
-        if( !$this->attributes[ 'country_icon' ] ) {
-            return false;
-        }
-
-        $imagePath = public_path('vendor/blade-flags/' . $this->attributes[ 'country_icon' ] );
-        return File::exists($imagePath);
     }
 }
--- database/seeders/CountryImageIconSeeder.php
+++ database/seeders/CountryImageIconSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Contract;
 use App\Models\Country;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\{
     Crypt,
     DB,
@@ -26,10 +27,25 @@ class CountryImageIconSeeder extends Seeder
     {
         $countries = Country::all();
         foreach( $countries as $c ) {
+            
             $code = Str::lower( $c->iso_alpha2_code );
-            $c->country_icon = 'country-' . $code . '.svg';
-            $c->country_image = $c->iso_alpha2_code . '.png';
+            $country_icon = 'country-' . $code . '.svg';
+            $country_image = $c->iso_alpha2_code . '.png';
+
+            $c->country_icon = self::getGotIconAttribute( $country_icon ) ? asset('vendor/blade-flags/' . $country_icon ) : '';
+            $c->country_image = self::getGotImageAttribute( $country_image ) ? asset('country/image_medium/' . $country_image ) : '';
             $c->save();
         }
+    }
+
+    public function getGotImageAttribute( $country_image ) {
+        $imagePath = public_path('country/image_medium/' . $country_image );
+        return File::exists($imagePath);
+    }
+
+    public function getGotIconAttribute( $country_icon ) {
+
+        $imagePath = public_path('vendor/blade-flags/' . $country_icon );
+        return File::exists($imagePath);
     }
 }
